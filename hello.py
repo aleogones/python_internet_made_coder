1a8ae58a67ae08ed56a8928554e6459bbc3b07a2
Aleogon <alberto.leon.gonzalez@aleogon.es>
2023-11-22 21:09:53 +0100

    primer commit

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-name = input("Introduce tu nombre: ")
+print('Hello, World!')
+
+name = input('Cual es tu nombre? ')
 print(f'Hola {name}')
+
+num1 = int(input('Introduce un numero: '))
+num2 = int(input('Introduce otro numero: '))
+Sum = num1+num2
+Dif = num1-num2
+Prod = num1*num2
+Div = num1/num2
+
+print(f'Sum:{Sum} Difference:{Dif} Product:{Prod} Quotient:{Div}')
+
